b2d5f2bdd54aff767f0b33dbef51671e6e5637b8
moe <muhammad.2712@outlook.com>
2020-01-16 17:52:39 +0000

    Switched to gorm schema

---
+++ database/schema.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// User ...
+type User struct {
+	gorm.Model
+	RoleID                int
+	Email                 string `gorm:"type:varchar(128);unique_index"`
+	Username              string
+	PasswordHash          string
+	Password              string
+	FirstName             string
+	LastName              string
+	IsConfirmed           string
+	IsActive              string
+	ResetPassToken        string
+	ResetPassTokenCreated string
+	LastLogin             string
+	FailedLogin           string
+	LockExpires           string
+}
+
+// Role ...
+type Role struct {
+	gorm.Model
+	Role        string `gorm:"type:varchar(32);unique_index"`
+	Permissions string
+}
+
+// TokenRevocation ...
+type TokenRevocation struct {
+	gorm.Model
+	UserID        int
+	RefreshToken  string
+	LogoutAll     bool
+	LogoutAllTime string
+}
+
+// Migrate ...
+func Migrate() {
+	defer db.Close()
+
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Role{})
+	db.AutoMigrate(&TokenRevocation{})
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.5.0
+	github.com/jinzhu/gorm v1.9.12
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.1
