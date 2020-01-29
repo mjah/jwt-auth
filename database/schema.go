23ab3a7af7a3de5c4281c5cd652587df60c0b516
moe <muhammad.2712@outlook.com>
2020-01-29 15:19:02 +0000

    Cleaned up database directory

--- database/schema.go
+++ database/schema.go
@@ -51,23 +51,3 @@ type EmailQueue struct {
 	MessageParameters string
 	ProcessedAt       *time.Time
 }
-
-// Migrate ...
-func Migrate() {
-	db := GetConnection()
-	defer db.Close()
-
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Role{})
-	db.AutoMigrate(&TokenRevocation{})
-	db.AutoMigrate(&EmailQueue{})
-
-	adminRole := &Role{Role: "Admin"}
-	db.FirstOrCreate(adminRole, adminRole)
-
-	memberRole := &Role{Role: "Member"}
-	db.FirstOrCreate(memberRole, memberRole)
-
-	guestRole := &Role{Role: "Guest"}
-	db.FirstOrCreate(guestRole, guestRole)
-}
