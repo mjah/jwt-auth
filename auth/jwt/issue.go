2643046bfa168682afce6a7f5853251ab59bb5de
moe <muhammad.2712@outlook.com>
2020-02-01 18:12:12 +0000

    Added token expiration through config

--- auth/jwt/issue.go
+++ auth/jwt/issue.go
@@ -4,13 +4,16 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/spf13/viper"
 )
 
 const issuer = "jwt-auth-server"
-const expireIn = time.Hour * 24
+
+var expireIn time.Duration
 
 // IssueAccessToken ...
 func IssueAccessToken() (string, error) {
+	expireIn = viper.GetDuration("token.access_token_expires")
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 		"iss": issuer,
 		"exp": time.Now().Add(expireIn).Unix(),
@@ -21,11 +24,11 @@ func IssueAccessToken() (string, error) {
 
 // IssueRefreshToken ...
 func IssueRefreshToken(extendedRefresh bool) (string, error) {
-	// if extendedRefresh {
-	// 	expireIn := viper
-	// } else {
-	// 	expireIn :=
-	// }
+	if extendedRefresh {
+		expireIn = viper.GetDuration("token.refresh_token_expires_extended")
+	} else {
+		expireIn = viper.GetDuration("token.refresh_token_expires")
+	}
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 		"iss": issuer,
 		"exp": time.Now().Add(expireIn).Unix(),
