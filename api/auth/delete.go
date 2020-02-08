f3223f03e7ebe6ced7fb11a8739339a64bdd0e8b
moe <muhammad.2712@outlook.com>
2020-02-08 15:06:37 +0000

    Implemented account deletion

--- api/auth/delete.go
+++ api/auth/delete.go
@@ -4,11 +4,20 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mjah/jwt-auth/auth"
+	"github.com/mjah/jwt-auth/auth/jwt"
 )
 
 // Delete ...
 func Delete(c *gin.Context) {
+	refreshTokenClaims := c.MustGet("refresh_token_claims").(jwt.RefreshTokenClaims)
+
+	if errCode := auth.Delete(refreshTokenClaims); errCode != nil {
+		c.AbortWithStatusJSON(errCode.HTTPStatus, gin.H{"message": errCode.OmitDetailsInProd()})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
+		"message": "Account deleted.",
 	})
 }
--- auth/delete.go
+++ auth/delete.go
@@ -1,1 +1,23 @@
 package auth
+
+import (
+	"github.com/mjah/jwt-auth/auth/jwt"
+	"github.com/mjah/jwt-auth/database"
+	"github.com/mjah/jwt-auth/errors"
+)
+
+// Delete ...
+func Delete(refreshTokenClaims jwt.RefreshTokenClaims) *errors.ErrorCode {
+	// Get database connection
+	db, err := database.GetConnection()
+	if err != nil {
+		return errors.New(errors.DatabaseConnectionFailed, err)
+	}
+
+	// Delete account
+	if err := db.Unscoped().Where("id = ?", refreshTokenClaims.UserID).Delete(&database.User{}).Error; err != nil {
+		return errors.New(errors.DatabaseQueryFailed, err)
+	}
+
+	return nil
+}
